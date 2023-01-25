613f8360a114de1d4e14a7a3146b40150b26c60b
knd <knd@knd.cloud>
2023-01-25 22:23:02 +0900

    fix ffmpeg_setup

--- sh/ffmpeg-setup.sh
+++ sh/ffmpeg-setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+printf "password: "
+read password
 
-sudo apt-get update -qq && sudo apt-get -y install \
+echo "$password" | sudo -S apt-get update -qq && sudo apt-get -y install \
   autoconf automake build-essential cmake git-core libass-dev \
   libfreetype6-dev libtool pkg-config texinfo wget zlib1g-dev
 sudo apt -y install nasm libx264-dev libnuma-dev libx265-dev libvpx-dev libfdk-aac-dev libmp3lame-dev libopus-dev libaom-dev \
@@ -12,7 +14,7 @@ cd ~/ffmpeg_sources && \
   cd SVT-AV1/build && \
   cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DBUILD_DEC=OFF -DBUILD_SHARED_LIBS=OFF .. && \
   make -j4 && \
-  sudo make install && \
+  echo "$password" | sudo -S make install && \
   cd
 
 cd ~/ffmpeg_sources/ && \
@@ -39,5 +41,5 @@ cd ffmpeg && \
   --enable-openssl \
   --enable-nonfree && \
 make -j4 && \
-sudo make install && \
+echo "$password" | sudo -S make install && \
 hash -r
